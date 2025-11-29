2968a2f5bf65455a3342440a8fef9cb7dc5991bb
burningtonemwatha <burnnymwatha@gmail.com>
2025-11-29 11:48:17 +0300

    Media app done

--- media_assets/urls.py
+++ media_assets/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('my-media/', views.my_media_view, name='my_media'),
     path('upload/', views.upload_view, name='upload_media'),
     path('media/<int:pk>/', views.media_detail_view, name='media_detail'),
-    path('media/<int:pk>/delete/', views.delete_media_view, name='delete_view'),
+    path('media/<int:pk>/delete/', views.delete_media_view, name='delete_media'),
     path('media/<int:pk>/edit/', views.edit_media_view, name='edit_media'),
 ]
